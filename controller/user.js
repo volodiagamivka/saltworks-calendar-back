477f02cea99111744da680ee2768f8fc2d1797d5
GoodKn1ght <oleh.sokal.shi.2023@lpnu.ua>
2025-02-10 11:56:49 +0200

    fixing controller

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db'); 
+const cors = require('cors');
+const app = express();
+
+app.use(cors({
+    origin: '*',  // â ïğîäàêøåí³ çàì³í³òü íà êîíêğåòíèé äîìåí
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false  // çì³íşºìî íà false, îñê³ëüêè âèêîğèñòîâóºìî origin: '*'
+}));
+app.options('*', cors());
+const db = require('../db');
 //http://localhost:3000/api/booking/
 router.get('/us', function (req, res) {
     const { timing, guide_name } = req.body;
@@ -9,7 +19,7 @@ router.get('/us', function (req, res) {
         .catch(err => res.send(err));
 });
 router.post('/calendar', function (req, res) {
-    const { phone, timing, adults, guide_id, kids, is_individual } = req.body; 
+    const { phone, timing, adults, guide_id, kids, is_individual } = req.body;
     db.query("CALL AddBooking(?, ?, ?, ?, ?, ?)", [phone, timing, adults, guide_id, kids, is_individual])
         .then(data => res.send(data))
         .catch(err => res.send(err));
@@ -20,20 +30,20 @@ router.get('/calendar', function (req, res) {
         .catch(err => res.send(err));
 });
 router.post('/admin', function (req, res) {
-    const { timing, guide_name } = req.body; 
+    const { timing, guide_name } = req.body;
     db.query("CALL AddNewTime(?, ?)", [timing, guide_name])
         .then(data => res.send(data))
         .catch(err => res.send(err));
 });
 
 router.delete('/user', function (req, res) {
-    const { booking_id } = req.body; 
+    const { booking_id } = req.body;
     db.query("CALL DeleteArrangement(?)", [booking_id])
         .then(data => res.send(data))
         .catch(err => res.send(err));
 });
 router.get('/user', function (req, res) {
-    const { phone } = req.body; 
+    const { phone } = req.body;
     db.query("CALL GetBookingsByPhoneNumber(?)", [phone])
         .then(data => res.send(data))
         .catch(err => res.send(err));
@@ -41,12 +51,12 @@ router.get('/user', function (req, res) {
 
 router.put('/user', function (req, res) {
     const { booking_id, new_adults, new_children } = req.body;
-    db.query("CALL ÑhangeArrangement(?, ?, ?)", [booking_id, new_adults, new_children])
+    db.query("CALL ?hangeArrangement(?, ?, ?)", [booking_id, new_adults, new_children])
         .then(data => res.send(data))
         .catch(err => res.send(err));
 });
 
-router.post('/admin', function (req, res) {
+router.post('/calendar', function (req, res) {
     const { timing, guide_name } = req.body;
     db.query("CALL AddNewTime(?, ?)", [timing, guide_name])
         .then(data => res.send(data))
--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 var express = require('express');
 var app = express();
+var cors = require('cors'); // Додаємо CORS
+
+// Налаштування CORS
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 var db = require('./db');
-var UserRoutes = require('./controller/user'); // Added 'var' keyword
+var UserRoutes = require('./controller/user');
+
 //middleware
 app.use('/api/booking', UserRoutes);
+
 //first make sure db connection is successful
 //then start the express server
 db.query("Select * from booking")
     .then(function (data) {
         console.log("succeeded");
-        app.listen(3000, function() {
+        app.listen(3000, function () {
             console.log('server started at 3000');
         });
     })
     .catch(function (err) {
         console.log('failed');
+        console.error('Database connection error:', err);
     });
+
+// Обробка помилок
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
