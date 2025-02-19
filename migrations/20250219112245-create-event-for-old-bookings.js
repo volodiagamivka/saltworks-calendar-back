67c8b51699b40aa3ba5cdedda862c2cf7a81b2e8
GoodKn1ght <oleh.sokal.shi.2023@lpnu.ua>
2025-02-19 13:25:21 +0200

    added event for deletion of past bookings and timings

---
+++ migrations/20250219112245-create-event-for-old-bookings.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.sequelize.query(`
+      CREATE EVENT delete_old_bookings
+      ON SCHEDULE EVERY 1 HOUR
+      DO
+      BEGIN
+        -- Delete old bookings
+        DELETE FROM booking
+        WHERE timing_id IN (
+          SELECT id
+          FROM timing
+          WHERE timing < NOW()
+        );
+
+        -- Delete old timing entries
+        DELETE FROM timing
+        WHERE timing < NOW();
+      END;
+    `);
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.sequelize.query('DROP EVENT IF EXISTS delete_old_bookings');
+    }
+};
